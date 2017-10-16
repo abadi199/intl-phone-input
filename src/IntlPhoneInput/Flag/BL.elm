777b43c09538fb1c67b7d86a73dff21be8494824
Abadi Kurniawan <abadi.kurniawan@gmail.com>
2017-10-16 14:13:43 -0500

    Add empty flag files

---
+++ src/IntlPhoneInput/Flag/BL.elm
@@ -0,0 +1,10 @@
+module IntlPhoneInput.Flag.BL exposing (flag)
+
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+
+
+flag : Svg msg
+flag =
+    svg [ width "100%", height "100%", viewBox "0 0 640 480" ]
+        [ rect [ fill "#aaa", x "0", y "0", width "640", height "480" ] [] ]
