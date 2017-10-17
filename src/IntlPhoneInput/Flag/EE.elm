1aab616ebcbf55e76d19d0f0513bd57d7464cec6
Abadi Kurniawan <abadi.kurniawan@gmail.com>
2017-10-16 23:28:41 -0500

    Finish 'E' flags

--- src/IntlPhoneInput/Flag/EE.elm
+++ src/IntlPhoneInput/Flag/EE.elm
@@ -6,5 +6,13 @@ import Svg.Attributes exposing (..)
 
 flag : Svg msg
 flag =
-    svg [ width "100%", height "100%", viewBox "0 0 640 480" ]
-        [ rect [ fill "#aaa", x "0", y "0", width "640", height "480" ] [] ]
+    svg [ height "100%", width "100%", viewBox "0 0 640 480" ]
+        [ g [ fillRule "evenodd", strokeWidth "1pt" ]
+            [ rect [ rx "0", ry "0", height "477.9", width "100%" ]
+                []
+            , rect [ rx "0", ry "0", height "159.3", width "100%", y "320.7", fill "#fff" ]
+                []
+            , Svg.path [ fill "#1291ff", d "M0 0h640v159.3H0z" ]
+                []
+            ]
+        ]
