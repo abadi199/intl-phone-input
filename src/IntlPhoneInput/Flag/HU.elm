974312c0bd1a8c8b28852828b4a71fee39f3ab9f
Abadi Kurniawan <abadi.kurniawan@gmail.com>
2017-10-16 23:07:50 -0500

    Finish 'H' flags

--- src/IntlPhoneInput/Flag/HU.elm
+++ src/IntlPhoneInput/Flag/HU.elm
@@ -6,5 +6,13 @@ import Svg.Attributes exposing (..)
 
 flag : Svg msg
 flag =
-    svg [ width "100%", height "100%", viewBox "0 0 640 480" ]
-        [ rect [ fill "#aaa", x "0", y "0", width "640", height "480" ] [] ]
+    svg [ height "100%", width "100%", viewBox "0 0 640 480" ]
+        [ g [ fillRule "evenodd" ]
+            [ Svg.path [ fill "#fff", d "M640.006 479.994H0V0h640.006z" ]
+                []
+            , Svg.path [ fill "#388d00", d "M640.006 479.994H0V319.996h640.006z" ]
+                []
+            , Svg.path [ fill "#d43516", d "M640.006 160.127H0V.13h640.006z" ]
+                []
+            ]
+        ]
