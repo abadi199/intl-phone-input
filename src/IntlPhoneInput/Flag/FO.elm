f64e69d0687ab75afc6c9e8e0ca231c8851553da
Abadi Kurniawan <abadi.kurniawan@gmail.com>
2017-10-16 23:25:25 -0500

    Finish 'F' flags

--- src/IntlPhoneInput/Flag/FO.elm
+++ src/IntlPhoneInput/Flag/FO.elm
@@ -6,5 +6,19 @@ import Svg.Attributes exposing (..)
 
 flag : Svg msg
 flag =
-    svg [ width "100%", height "100%", viewBox "0 0 640 480" ]
-        [ rect [ fill "#aaa", x "0", y "0", width "640", height "480" ] [] ]
+    svg [ height "100%", width "100%", viewBox "0 0 640 480" ]
+        [ defs []
+            [ Svg.clipPath [ id "a" ]
+                [ Svg.path [ fillOpacity ".67", d "M-78.015 32h640v480h-640z" ]
+                    []
+                ]
+            ]
+        , g [ fillRule "evenodd", Svg.Attributes.clipPath "url(#a)", transform "translate(78.02 -32)", strokeWidth "0" ]
+            [ Svg.path [ fill "#fff", d "M-78.015 32h663.91v480h-663.91z" ]
+                []
+            , Svg.path [ d "M-76.033 218.67h185.9V32h106.23v186.67h371.79v106.67h-371.79v186.67h-106.23V325.34h-185.9V218.67z", fill "#003897" ]
+                []
+            , Svg.path [ d "M-76.033 245.33h212.45V32h53.113v213.33h398.35v53.333H189.53v213.33h-53.113v-213.33h-212.45V245.33z", fill "#d72828" ]
+                []
+            ]
+        ]
--- src/IntlPhoneInput/Flag/FR.elm
+++ src/IntlPhoneInput/Flag/FR.elm
@@ -6,5 +6,13 @@ import Svg.Attributes exposing (..)
 
 flag : Svg msg
 flag =
-    svg [ width "100%", height "100%", viewBox "0 0 640 480" ]
-        [ rect [ fill "#aaa", x "0", y "0", width "640", height "480" ] [] ]
+    svg [ height "100%", width "100%", viewBox "0 0 640 480" ]
+        [ g [ fillRule "evenodd", strokeWidth "1pt" ]
+            [ Svg.path [ fill "#fff", d "M0 0h640v480H0z" ]
+                []
+            , Svg.path [ fill "#00267f", d "M0 0h213.337v480H0z" ]
+                []
+            , Svg.path [ fill "#f31830", d "M426.662 0H640v480H426.662z" ]
+                []
+            ]
+        ]
