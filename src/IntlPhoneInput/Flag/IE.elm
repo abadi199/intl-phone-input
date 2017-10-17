fec6d194d82d411246df15324384d1338ae6a33b
Abadi Kurniawan <abadi.kurniawan@gmail.com>
2017-10-16 23:06:07 -0500

    Finish 'I' flags

--- src/IntlPhoneInput/Flag/IE.elm
+++ src/IntlPhoneInput/Flag/IE.elm
@@ -6,5 +6,13 @@ import Svg.Attributes exposing (..)
 
 flag : Svg msg
 flag =
-    svg [ width "100%", height "100%", viewBox "0 0 640 480" ]
-        [ rect [ fill "#aaa", x "0", y "0", width "640", height "480" ] [] ]
+    svg [ height "100%", width "100%", viewBox "0 0 640 480" ]
+        [ g [ fillRule "evenodd", strokeWidth "1pt" ]
+            [ Svg.path [ fill "#fff", d "M0 0h639.995v480.004H0z" ]
+                []
+            , Svg.path [ fill "#009A49", d "M0 0h213.334v480.004H0z" ]
+                []
+            , Svg.path [ fill "#FF7900", d "M426.668 0h213.334v480.004H426.668z" ]
+                []
+            ]
+        ]
--- src/IntlPhoneInput/Flag/IS.elm
+++ src/IntlPhoneInput/Flag/IS.elm
@@ -6,5 +6,19 @@ import Svg.Attributes exposing (..)
 
 flag : Svg msg
 flag =
-    svg [ width "100%", height "100%", viewBox "0 0 640 480" ]
-        [ rect [ fill "#aaa", x "0", y "0", width "640", height "480" ] [] ]
+    svg [ height "100%", width "100%", viewBox "0 0 640 480" ]
+        [ defs []
+            [ Svg.clipPath [ id "a" ]
+                [ Svg.path [ fillOpacity ".67", d "M0 0h640v480H0z" ]
+                    []
+                ]
+            ]
+        , g [ fillRule "evenodd", strokeWidth "0", Svg.Attributes.clipPath "url(#a)" ]
+            [ Svg.path [ fill "#003897", d "M0 0h666.67v480H0z" ]
+                []
+            , Svg.path [ d "M0 186.67h186.67V0h106.67v186.67h373.33v106.67H293.34v186.67H186.67V293.34H0V186.67z", fill "#fff" ]
+                []
+            , Svg.path [ d "M0 213.33h213.33V0h53.333v213.33h400v53.333h-400v213.33H213.33v-213.33H0V213.33z", fill "#d72828" ]
+                []
+            ]
+        ]
